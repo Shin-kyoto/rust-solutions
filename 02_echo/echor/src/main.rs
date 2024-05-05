525b077a91e64eee43fcf83078549a1dda4f1928
Shin-kyoto <aquashin0202@gmail.com>
2024-05-05 12:28:37 +0900

    feat: add example for args
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

---
+++ 02_echo/echor/src/main.rs
@@ -0,0 +1,11 @@
+use clap::App;
+
+fn main() {
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .about("Rust echo")
+        .author("Shintaro Tomie")
+        .get_matches();
+    // println!("{:?}", std::env::args());
+    // println!("Hello, world!");
+}
