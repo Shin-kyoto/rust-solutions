4d810f9480de7e00071dfc5a85e7de313d29a02b
Shin-kyoto <aquashin0202@gmail.com>
2024-05-06 20:13:48 +0900

    feat: add output for args info
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- 02_echo/echor/src/main.rs
+++ 02_echo/echor/src/main.rs
@@ -1,11 +1,8 @@
 use clap::App;
 
 fn main() {
-    let _matches = App::new("echor")
-        .version("0.1.0")
-        .about("Rust echo")
-        .author("Shintaro Tomie")
-        .get_matches();
+    let _matches = App::new("echor").version("0.1.0").about("Rust echo").author("Shintaro Tomie").get_matches();
     // println!("{:?}", std::env::args());
     // println!("Hello, world!");
+    println!("{:#?}", _matches);
 }
