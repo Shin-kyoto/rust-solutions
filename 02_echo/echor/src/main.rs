ac1f09d01642485e2dce9113c3e9581a2883ca17
Shin-kyoto <aquashin0202@gmail.com>
2024-05-07 02:10:24 +0900

    feat: add options for text input and newline omitting options
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- 02_echo/echor/src/main.rs
+++ 02_echo/echor/src/main.rs
@@ -1,8 +1,22 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor").version("0.1.0").about("Rust echo").author("Shintaro Tomie").get_matches();
+    let matches = App::new("echor").version("0.1.0").about("Rust echo").author("Shintaro Tomie")
+        .arg(
+            Arg::with_name("text").value_name("TEXT").help("input text").required(true).min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline").short("n").help("Do not print newline").takes_value(false),
+        )
+        .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    let mut ending = "\n";
+    if omit_newline {
+        ending = "";
+    }
     // println!("{:?}", std::env::args());
-    // println!("Hello, world!");
-    println!("{:#?}", _matches);
+    // println!("{:#?}", _matches);
+    println!("{}{}", text.join(""), ending);
 }
