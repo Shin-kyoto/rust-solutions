c80342a91c9ffe85ea95466da3b1be5528eb4cd7
Shin-kyoto <aquashin0202@gmail.com>
2024-05-09 22:51:51 +0900

    feat: update test for hello1,2
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- 02_echo/echor/tests/cli.rs
+++ 02_echo/echor/tests/cli.rs
@@ -4,6 +4,16 @@ use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
 #[test]
 fn dies_no_args() -> TestResult{
     // let mut cmd = Command::cargo_bin("echor").unwrap();
@@ -21,11 +31,10 @@ fn runs() {
 
 #[test]
 fn hello1() -> TestResult {
-    let outfile = "tests/expected/hello1.txt";
-    // let expected = fs::read_to_string(outfile).unwrap();
-    // let mut cmd = Command::cargo_bin("echor").unwrap();
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
 }
